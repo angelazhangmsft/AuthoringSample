0d95765efe0ecd097b59f4b612fd39877226af3c
Angela Zhang <angzhang@microsoft.com>
2020-10-21 16:16:43 -0400

    Add project files.

---
+++ AuthoringSample/PackageFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuthoringSample
+{
+    public class PackageFinder
+    {
+        //List<string> packageList = new List<string>();
+
+        public static Uri GetUri()
+        {
+            return new Uri("www.microsoft.com");
+        }
+
+        //public List<string> getPackageList(string path)
+        //{
+        //    return packageList;
+        //}
+
+    }
+}
